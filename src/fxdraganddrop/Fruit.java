988611266771cf25de6c3619f839f872801aa643
ddren <ddren@LAPTOP-F0MJINGF.modem>
2020-11-19 20:24:03 +1100

    First build

---
+++ src/fxdraganddrop/Fruit.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fxdraganddrop;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ddren
+ */
+public class Fruit  implements Serializable
+{
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String name = "";
+
+	public Fruit(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	@Override
+	public String toString()
+	{
+		return name;
+	}
+}
